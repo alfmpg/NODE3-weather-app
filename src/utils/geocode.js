ba8ba9a442d3acd3c8b391a13457fbc3a5aeae88
Alfonso Miguel Palencia <alfmpg@gmail.com>
2020-04-13 12:38:34 +0200

    Solucionado modulo geocode

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,12 +1,23 @@
 const request = require('request')
 
+//Solución para sustituir la propiedad length
+Object.size = function (obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+}
+
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYWxmbXBnIiwiYSI6ImNrOHN5cXUzczBrYzMza2xvYzNzZXpibHcifQ.mL9kJ-Z8mA_pxT2UzbiQkw&limit=1'
     request({ url, json: true }, (error, {body}) => {
+        var size = Object.size(body.features);
+        console.log(size)
         if (error) {
             callback('Unable to connect to location services!', undefined)
 //Aqui es donde me falla no puedo poner body.features.length === 0
-        } else if (body.features.error > 0) {
+        } else if (size === 0) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
@@ -17,5 +28,7 @@ const geocode = (address, callback) => {
         }
     })
 }
+
+
 
 module.exports = geocode
